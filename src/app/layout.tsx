d5b5568958562817ae2420125114f1feb68e8588
Steyn Dullens <steyn@macbook-air.home>
2025-10-26 23:52:10 +0100

    Fix hydration error: restructure layout with proper flex container and context providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,18 +65,20 @@ export default function RootLayout({
           <AuthProvider>
             <LanguageProvider>
               <RecipeProvider>
-                {/* Desktop Sidebar */}
-                <DesktopSidebar />
+                <div className="flex">
+                  {/* Desktop Sidebar */}
+                  <DesktopSidebar />
+                  
+                  {/* Main content area */}
+                  <main className="main-content flex-1">
+                    <div className="max-w-7xl mx-auto p-4 lg:p-8">
+                      {children}
+                    </div>
+                  </main>
+                </div>
                 
                 {/* Mobile Navigation */}
                 <SimpleFloatingNav />
-                
-                {/* Main content area */}
-                <main className="main-content">
-                  <div className="max-w-7xl mx-auto p-4 lg:p-8">
-                    {children}
-                  </div>
-                </main>
               </RecipeProvider>
             </LanguageProvider>
           </AuthProvider>
